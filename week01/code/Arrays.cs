37b09e2d06420af6a55a547e0724a262ae471a3b
gkztech <maccarthy28@gmail.com>
2026-03-05 10:26:50 -0800

    Week 01 Done

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,20 +1,31 @@
 public static class Arrays
 {
+    
     /// <summary>
     /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
     /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    
+     // This function creates an array of multiples of 'start'
+    // 'count' defines how many multiples to generate
+    
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+       // Step 1: Create a new array to hold the multiples
+    double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+    // Step 2: Loop through 'length' times to fill the array
+    for (int i = 0; i < length; i++)
+    {
+        // Step 3: Multiply number by (i + 1) to get each multiple
+        multiples[i] = number * (i + 1);
     }
+
+    // Step 4: Return the array containing all multiples
+    return multiples;
+}
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -23,11 +34,27 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+    
+    // This function rotates a list to the right by 'amount'
+    // Elements that move past the end wrap around to the front
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Step 1: Calculate the starting index of the rotated portion
+        int startIndex = data.Count - amount;
+
+        // Step 2: Get the last 'amount' elements that will move to the front
+        List<int> rotatedPart = data.GetRange(startIndex, amount);
+
+        // Step 3: Get the remaining elements that will shift to the back
+        List<int> remainingPart = data.GetRange(0, startIndex);
+
+        // Step 4: Clear the original list
+        data.Clear();
+
+        // Step 5: Add rotated elements first, then the remaining elements
+        data.AddRange(rotatedPart);
+        data.AddRange(remainingPart);
     }
+
+    // Any other methods in this class remain unchanged
 }
